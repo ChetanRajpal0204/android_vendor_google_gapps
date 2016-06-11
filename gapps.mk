bef895089571a853f0f7cd4dc693b3da790f4043
udK <privacy@none.org>
2016-06-11 13:45:59 +0200

    First commit
    * WebView
    * Exchange
    * Keyboard
    * Chrome
    * Gmail
    * Google Play
    * YouTube
    * Play Services
    
    Signed-off-by: udK <privacy@none.org>

---
+++ gapps.mk
@@ -0,0 +1,124 @@
+# WebViewGoogle
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/WebViewGoogle/WebViewGoogle.apk:system/app/WebViewGoogle/WebViewGoogle.apk \
+    vendor/google/gapps/system/app/WebViewGoogle/lib/arm/libwebviewchromium.so:system/app/WebViewGoogle/lib/arm/libwebviewchromium.so
+
+# Exchange
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/PrebuiltExchange3Google/PrebuiltExchange3Google.apk:system/app/PrebuiltExchange3Google/PrebuiltExchange3Google.apk
+
+# Keyboard
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/LatinImeGoogle/LatinImeGoogle.apk:system/app/LatinImeGoogle/LatinImeGoogle.apk \
+    vendor/google/gapps/system/app/LatinImeGoogle/lib/arm/libjni_delight4decoder.so:system/app/LatinImeGoogle/lib/arm/libjni_delight4decoder.so
+
+# Chrome
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/Chrome/Chrome.apk:system/app/Chrome/Chrome.apk \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1847.114.so:system/app/Chrome/lib/arm/libchrome.1847.114.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1916.122.so:system/app/Chrome/lib/arm/libchrome.1916.122.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1916.138.so:system/app/Chrome/lib/arm/libchrome.1916.138.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1916.141.so:system/app/Chrome/lib/arm/libchrome.1916.141.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1985.122.so:system/app/Chrome/lib/arm/libchrome.1985.122.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1985.128.so:system/app/Chrome/lib/arm/libchrome.1985.128.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1985.131.so:system/app/Chrome/lib/arm/libchrome.1985.131.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.1985.135.so:system/app/Chrome/lib/arm/libchrome.1985.135.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.2062.117.so:system/app/Chrome/lib/arm/libchrome.2062.117.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.2125.102.so:system/app/Chrome/lib/arm/libchrome.2125.102.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.2125.114.so:system/app/Chrome/lib/arm/libchrome.2125.114.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.2171.37.so:system/app/Chrome/lib/arm/libchrome.2171.37.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchrome.2171.59.so:system/app/Chrome/lib/arm/libchrome.2171.59.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchromeview.so:system/app/Chrome/lib/arm/libchromeview.so \
+    vendor/google/gapps/system/app/Chrome/lib/arm/libchromium_android_linker.so:system/app/Chrome/lib/arm/libchromium_android_linker.so
+
+# Gmail
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/Gmail/Gmail.apk:system/app/Gmail/Gmail.apk
+
+# Google Play
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/priv-app/Phonesky/Phonesky.apk:system/priv-app/Phonesky/Phonesky.apk
+
+# YouTube
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/app/YouTube/YouTube.apk:system/app/YouTube/YouTube.apk \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libambisonic_audio_renderer.so:system/app/YouTube/lib/arm/libambisonic_audio_renderer.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libcronet.so:system/app/YouTube/lib/arm/libcronet.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libfilterframework_jni.so:system/app/YouTube/lib/arm/libfilterframework_jni.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libframesequence.so:system/app/YouTube/lib/arm/libframesequence.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libluajit.so:system/app/YouTube/lib/arm/libluajit.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libmoxie.so:system/app/YouTube/lib/arm/libmoxie.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libmoxieclient.so:system/app/YouTube/lib/arm/libmoxieclient.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libvpx.so:system/app/YouTube/lib/arm/libvpx.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libvpxJNI.so:system/app/YouTube/lib/arm/libvpxJNI.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libvrtoolkit.so:system/app/YouTube/lib/arm/libvrtoolkit.so \
+    vendor/google/gapps/system/app/YouTube/lib/arm/libwebp_android.so:system/app/YouTube/lib/arm/libwebp_android.so
+
+# Google Play Services
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so:system/priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libgmscore.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgmscore.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libgoogle-ocrclient-v3.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgoogle-ocrclient-v3.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libjgcastservice.so:system/priv-app/PrebuiltGmsCore/lib/arm/libjgcastservice.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libleveldbjni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libleveldbjni.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libNearbyApp.so:system/priv-app/PrebuiltGmsCore/lib/arm/libNearbyApp.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libwearable-selector.so:system/priv-app/PrebuiltGmsCore/lib/arm/libwearable-selector.so \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore/lib/arm/libWhisperDev.so:system/priv-app/PrebuiltGmsCore/lib/arm/libWhisperDev.so
+
+# ConfigUpdater
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/priv-app/ConfigUpdater/ConfigUpdater.apk:system/priv-app/ConfigUpdater/ConfigUpdater.apk
+
+# Core files
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/priv-app/GoogleBackupTransport/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport/GoogleBackupTransport.apk \
+    vendor/google/gapps/system/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk \
+    vendor/google/gapps/system/priv-app/GoogleLoginService/GoogleLoginService.apk:system/priv-app/GoogleLoginService/GoogleLoginService.apk \
+    vendor/google/gapps/system/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk \
+    vendor/google/gapps/system/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
+
+PRODUCT_COPY_FILES += \
+    vendor/google/gapps/system/usr/srec/en-US/action.pumpkin:system/usr/srec/en-US/action.pumpkin \
+    vendor/google/gapps/system/usr/srec/en-US/c_fst:system/usr/srec/en-US/c_fst \
+    vendor/google/gapps/system/usr/srec/en-US/class_normalizer.mfar:system/usr/srec/en-US/class_normalizer.mfar \
+    vendor/google/gapps/system/usr/srec/en-US/CLG.prewalk.fst:system/usr/srec/en-US/CLG.prewalk.fst \
+    vendor/google/gapps/system/usr/srec/en-US/commands.abnf:system/usr/srec/en-US/commands.abnf \
+    vendor/google/gapps/system/usr/srec/en-US/compile_grammar.config:system/usr/srec/en-US/compile_grammar.config \
+    vendor/google/gapps/system/usr/srec/en-US/config.pumpkin:system/usr/srec/en-US/config.pumpkin \
+    vendor/google/gapps/system/usr/srec/en-US/contacts.abnf:system/usr/srec/en-US/contacts.abnf \
+    vendor/google/gapps/system/usr/srec/en-US/CONTACTS.fst:system/usr/srec/en-US/CONTACTS.fst \
+    vendor/google/gapps/system/usr/srec/en-US/CONTACTS.syms:system/usr/srec/en-US/CONTACTS.syms \
+    vendor/google/gapps/system/usr/srec/en-US/dict:system/usr/srec/en-US/dict \
+    vendor/google/gapps/system/usr/srec/en-US/dictation.config:system/usr/srec/en-US/dictation.config \
+    vendor/google/gapps/system/usr/srec/en-US/dist_belief:system/usr/srec/en-US/dist_belief \
+    vendor/google/gapps/system/usr/srec/en-US/dnn:system/usr/srec/en-US/dnn \
+    vendor/google/gapps/system/usr/srec/en-US/endpointer_dictation.config:system/usr/srec/en-US/endpointer_dictation.config \
+    vendor/google/gapps/system/usr/srec/en-US/endpointer_model.mmap:system/usr/srec/en-US/endpointer_model.mmap \
+    vendor/google/gapps/system/usr/srec/en-US/endpointer_voicesearch.config:system/usr/srec/en-US/endpointer_voicesearch.config \
+    vendor/google/gapps/system/usr/srec/en-US/g2p.data:system/usr/srec/en-US/g2p.data \
+    vendor/google/gapps/system/usr/srec/en-US/g2p_fst:system/usr/srec/en-US/g2p_fst \
+    vendor/google/gapps/system/usr/srec/en-US/grammar.config:system/usr/srec/en-US/grammar.config \
+    vendor/google/gapps/system/usr/srec/en-US/graphemes.syms:system/usr/srec/en-US/graphemes.syms \
+    vendor/google/gapps/system/usr/srec/en-US/hmmlist:system/usr/srec/en-US/hmmlist \
+    vendor/google/gapps/system/usr/srec/en-US/hmm_symbols:system/usr/srec/en-US/hmm_symbols \
+    vendor/google/gapps/system/usr/srec/en-US/input_mean_std_dev:system/usr/srec/en-US/input_mean_std_dev \
+    vendor/google/gapps/system/usr/srec/en-US/lexicon.U.fst:system/usr/srec/en-US/lexicon.U.fst \
+    vendor/google/gapps/system/usr/srec/en-US/lstm_model.uint8.data:system/usr/srec/en-US/lstm_model.uint8.data \
+    vendor/google/gapps/system/usr/srec/en-US/magic_mic.config:system/usr/srec/en-US/magic_mic.config \
+    vendor/google/gapps/system/usr/srec/en-US/metadata:system/usr/srec/en-US/metadata \
+    vendor/google/gapps/system/usr/srec/en-US/normalizer.mfar:system/usr/srec/en-US/normalizer.mfar \
+    vendor/google/gapps/system/usr/srec/en-US/norm_fst:system/usr/srec/en-US/norm_fst \
+    vendor/google/gapps/system/usr/srec/en-US/offensive_word_normalizer.mfar:system/usr/srec/en-US/offensive_word_normalizer.mfar \
+    vendor/google/gapps/system/usr/srec/en-US/phonelist:system/usr/srec/en-US/phonelist \
+    vendor/google/gapps/system/usr/srec/en-US/phonelist.syms:system/usr/srec/en-US/phonelist.syms \
+    vendor/google/gapps/system/usr/srec/en-US/phonemes.syms:system/usr/srec/en-US/phonemes.syms \
+    vendor/google/gapps/system/usr/srec/en-US/rescoring.fst.louds:system/usr/srec/en-US/rescoring.fst.louds \
+    vendor/google/gapps/system/usr/srec/en-US/semantics.pumpkin:system/usr/srec/en-US/semantics.pumpkin \
+    vendor/google/gapps/system/usr/srec/en-US/voice_actions.config:system/usr/srec/en-US/voice_actions.config \
+    vendor/google/gapps/system/usr/srec/en-US/voice_actions_compiler.config:system/usr/srec/en-US/voice_actions_compiler.config \
+    vendor/google/gapps/system/usr/srec/en-US/wordlist.syms:system/usr/srec/en-US/wordlist.syms
